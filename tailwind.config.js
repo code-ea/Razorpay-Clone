031a2fae73e5d74dff7c90e543324119eca45443
Nitin Singh <borntocode.ea@gmail.com>
2023-10-19 12:25:47 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
-/** @type {import('tailwindcss').Config} */
+@type {import('tailwindcss').Config}
 module.exports = {
-  content: ['./index.html'],
+  content: ["./index.html"],
   theme: {
     extend: {fontFamily: {
       mullish: ["Mulish", "sans-serif"],
